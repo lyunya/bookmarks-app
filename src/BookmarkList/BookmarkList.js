ea17dbc0c6458a01d2a8f8d097b8266c8780e8ee
Leon Marbukh <leonmarbukh@gmail.com>
2020-04-29 11:08:04 -0700

    hid API key in env

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
+import PropTypes from "prop-types"
 import './BookmarkList.css'
 
 class BookmarkList extends Component {
-  static defaultProps = {
-    bookmarks: []
-  };
+  static contextType = BookmarksContext;
+
 
   render() {
-    const { bookmarks } = this.props
+    const { bookmarks } = this.context;
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
@@ -24,5 +25,16 @@ class BookmarkList extends Component {
     );
   }
 }
+
+BookmarkList.propTypes = {
+  bookmarks: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      rating: PropTypes.number,
+      description: PropTypes.string,
+    })
+  ),
+};
 
 export default BookmarkList;
